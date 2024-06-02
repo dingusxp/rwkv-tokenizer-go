a31d4168268534991968b0e3791985083d418b1e
Ronsor <ronsor@ronsor.pw>
2024-06-01 23:09:45 -0700

    Run go fmt

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -20,17 +20,17 @@ func intSliceEquals(a, b []int) bool {
 // TestSimpleRoundtrip tests the creation of a tokenizer with the
 // default vocabulary and round-tripping a unicode string.
 func TestSimpleRoundtrip(t *testing.T) {
-        tkn := NewWorldTokenizer()
+	tkn := NewWorldTokenizer()
 
 	s := "Hello, world! こんにちは、世界！"
 	i := []int{33155, 45, 40213, 34, 33, 10115, 10165, 10136, 10127, 10139, 10079, 10267, 14610, 19126}
 
-        x, err := tkn.EncodeString(s)
+	x, err := tkn.EncodeString(s)
 	if !intSliceEquals(x, i) || err != nil {
 		t.Fatalf(`EncodeString(%q) = %v, %v, want equal to %v`, s, x, err, i)
 	}
 
-        y, err := tkn.DecodeToString(x)
+	y, err := tkn.DecodeToString(x)
 	if y != s || err != nil {
 		t.Fatalf(`DecodeToString(%v) = %q, %v, want equal to %q`, x, y, err, s)
 	}
